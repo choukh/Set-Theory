e28e2cb06f3ab07fbeda1f786424175557eabf84
choukh <choukyuhei@gmail.com>
2020-07-14 05:53:28 +0900

    delete wrong file

--- EST_5_5.v
+++
@@ -1,9 +0,0 @@
-(** Based on "Elements of Set Theory" Chapter 5 Part 5 **)
-(** Coq coding by choukh, July 2020 **)
-
-Require Export ZFC.CH5_1.
-
-(*** EST第五章5：实数的定义 ***)
-
-Close Scope Int_scope.
-Open Scope Rat_scope.
